e4263567ad056db41ebff0300ed6800397e7e554
HeinWessels(Personal) <heinwessels93@gmail.com>
2022-03-20 21:17:54 +0100

    Start processing data

--- draw-family-tree.py
+++ draw-family-tree.py
@@ -8,20 +8,22 @@ class Person:
     and links to the households person belongs to.
     Person doesn't know if person is male or female.
     '''
+    def __init__(self):
+        self.households = []
 
     @classmethod
     def from_csv_row(cls, args):
         person = cls()
-        person.id = args[0]
-        person.marker = args[1]
+        person.id = args[0] # Required
+        person.marker = args[1] if args[1] != "" else None
         person.name = args[2]
-        person.surname = args[3]
-        person.birth = args[4]
-        person.christened = args[5]
-        person.death = args[6]
-        person.parents = args[7].split(",")
-        person.spouse = args[8].split(",")
-        person.description = args[9]
+        person.surname = args[3] if args[3] != "" else None
+        person.birth = args[4] if args[4] != "" else None
+        person.christened = args[5] if args[5] != "" else None
+        person.death = args[6] if args[6] != "" else None
+        person.parents = args[7].split(",") if args[7] != "" else None
+        person.spouses = args[8].split(",") if args[8] != "" else None
+        person.description = args[9] if args[9] != "" else None
         return person
 
 class Household:
@@ -65,8 +67,31 @@ class Family:
     Process the list of people to generate all the
     valid connections.
     """
-    def generate():
-        pass
+    def generate(self):
+        
+        for _, person in self.people.items():
+
+            # First turn the temporary ID's used for 
+            # parents and spouses into person links
+            for index in range(len(person.spouses) if person.spouses else 0):
+                person.spouses[index] = self.people[person.spouses[index]]
+            for index in range(len(person.parents) if person.parents else 0):
+                person.parents[index] = self.people[person.parents[index]]
+
+            # We will create new households by looking at 
+            # every new person's parents.
+            if person.parents is not None:
+                
+                # This person needs to be part of an household
+                # Find the first parent, and join said household.
+                # Create if neccesary.
+                main_parent = person.parents[0]
+                for household in main_parent.households:
+                    # How do I know if this household belongs
+                    # to this person?
+                    pass
+                    
+
 
 if __name__ == "__main__":
 
