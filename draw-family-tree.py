566811642080f29c420149a0af91e8cce1c7c7e7
HeinWessels(Personal) <heinwessels93@gmail.com>
2022-03-22 21:49:58 +0100

    Rudimental recursive iteration

--- draw-family-tree.py
+++ draw-family-tree.py
@@ -8,8 +8,6 @@ class Person:
     and links to the households person belongs to.
     Person doesn't know if person is male or female.
     '''
-    def __init__(self):
-        self.households = []
 
     @classmethod
     def from_csv_row(cls, args):
@@ -21,9 +19,10 @@ class Person:
         person.birth = args[4] if args[4] != "" else None
         person.christened = args[5] if args[5] != "" else None
         person.death = args[6] if args[6] != "" else None
-        person.parents = args[7].split(",") if args[7] != "" else None
-        person.spouses = args[8].split(",") if args[8] != "" else None
-        person.description = args[9] if args[9] != "" else None
+        person.spouses = args[7].split(",") if args[7] != "" else None
+        person.household_as_child = args[8] if args[8] != "" else None
+        person.households_as_parent = args[9].split(",") if args[9] != "" else None
+        person.description = args[10] if args[10] != "" else None
         return person
 
 class Household:
@@ -39,11 +38,22 @@ class Household:
         self.parents = []
         self.children = []
 
+    def add_child(self, parent):
+        if parent in self.parents:
+            raise RuntimeError(f"Child {parent.id} already in household!")
+        self.children.append(parent)
+
+    def add_parent(self, parent):
+        if parent in self.parents:
+            raise RuntimeError(f"Parent {parent.id} already in household!")
+        self.parents.append(parent)
+
 class Family:
     '''
     Contains all people and all households
     '''
-    people = {}    # Dictionary of all people in the tree
+    people = {}     # Dictionary of all people in the tree
+    households = {} # Dictionary of all households
     
     '''
     Popualte the entire family tree with 
@@ -61,7 +71,6 @@ class Family:
                 self.people[person.id] = copy.deepcopy(person)
         
         # Done populating family with people
-        print(f"Imported {len(self.people)} people from:", path)
 
     """
     Process the list of people to generate all the
@@ -72,26 +81,52 @@ class Family:
         for _, person in self.people.items():
 
             # First turn the temporary ID's used for 
-            # parents and spouses into person links
+            # spouses into person links
             for index in range(len(person.spouses) if person.spouses else 0):
                 person.spouses[index] = self.people[person.spouses[index]]
-            for index in range(len(person.parents) if person.parents else 0):
-                person.parents[index] = self.people[person.parents[index]]
 
-            # We will create new households by looking at 
-            # every new person's parents.
-            if person.parents is not None:
-                
-                # This person needs to be part of an household
-                # Find the first parent, and join said household.
-                # Create if neccesary.
-                main_parent = person.parents[0]
-                for household in main_parent.households:
-                    # How do I know if this household belongs
-                    # to this person?
-                    pass
-                    
+            # Now turn households into links, and creating them if
+            # they don't exist already. We will do this for both
+            # households as parent and households as child
+            for index in range(len(person.households_as_parent) \
+                    if person.households_as_parent else 0):
+                if not person.households_as_parent[index] in self.households:
+                    self.households[person.households_as_parent[index]] = Household()
+                person.households_as_parent[index] = self.households[person.households_as_parent[index]]
+                person.households_as_parent[index].add_parent(person)
+            if person.household_as_child:
+                if not person.household_as_child in self.households:
+                    self.households[person.household_as_child] = Household()
+                person.household_as_child = self.households[person.household_as_child]
+                person.household_as_child.add_child(person)
+            
+    def draw(self, root_id):
 
+        # Find the root
+        if not root_id in self.people:
+            raise RuntimeError(f"Root ID '{root_id}' does not exist")
+        root = self.people[root_id]
+
+        # So far draw only the first household
+        self.draw_households_recusive(root.households_as_parent[0])
+
+    def draw_households_recusive(self, household, iteration=0):
+
+        # Draw all the parents for this household
+        print((iteration*" ") + " | ".join([parent.name for parent in household.parents]))
+
+        for child in household.children:
+            if child.households_as_parent:
+                self.draw_households_recusive(child.households_as_parent[0], iteration + 1)
+            else:
+                print(((iteration+1)*" ") + child.name)
+        
+            
+
+
+
+
+        
 
 if __name__ == "__main__":
 
@@ -99,3 +134,5 @@ if __name__ == "__main__":
 
     family.populate("output.csv")
     family.generate()
+
+    family.draw(root_id="a1b2c2d4e5f5g9")
